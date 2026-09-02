43f4a2f726a3d01ea6f12695785fcbd02cf3c839
alce09 <alce09001@utb.vaxjo.se>
2026-09-02 10:23:11 +0200

    Add project files.

---
+++ Uppgift1.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Uppgift_1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("     /\\");
+            Console.WriteLine("    /  \\");
+            Console.WriteLine("   /    \\");
+            Console.WriteLine("  /      \\");
+            Console.WriteLine(" /        \\");
+            Console.WriteLine("/__________\\");
+            
+
+        }
+
+
+    }
+
+
+}
+
